e5df695409ecfdf783a160b030c1cf3cd1ba043c
Nisa Oğuz <117512038+nisaoguz@users.noreply.github.com>
2023-02-16 15:02:38 +0300

    Create odev.sql
    dvdrental kütüphanesi sorgu örnekleri
    
    Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
    
    1-film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
    2-film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
    3-film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
    4-customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
    5-film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.

---
+++ work/odev.sql
@@ -0,0 +1,6 @@
+--1-- Select title,description  From film
+--2-- Select * From film Where length>60 AND length<75
+--3-- Select * From film Where rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99)
+--4-- Select last_name From customer Where first_name='Mary'
+--5-- Select * From film Where NOT length>50 AND NOT(rental_rate = 2.99 OR rental_rate = 4.99)
+
