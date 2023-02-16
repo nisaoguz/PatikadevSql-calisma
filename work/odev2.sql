3d8756d5896b3bf884bc4667822ccfa47c6028d6
Nisa Oğuz <117512038+nisaoguz@users.noreply.github.com>
2023-02-16 21:35:44 +0300

    Create odev2.sql
    Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
    
    film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
    .actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
    film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)

---
+++ work/odev2.sql
@@ -0,0 +1,3 @@
+--1-- Select * From film Where replacement_cost BETWEEN 12.99  AND 16.99   
+--2-- Select first_name,last_name  From actor Where first_name IN ('Penelope','Nick','Ed')
+--3-- Select * From film Where rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99)
